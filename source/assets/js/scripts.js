3b861cd93f19778d2280d3729207b76e149c17f5
jina <jina@sushiandrobots.com>
2016-01-05 03:13:35 -0800

    clean js

--- source/assets/js/scripts.js
+++ source/assets/js/scripts.js
@@ -1,12 +1,8 @@
 //= require      ./vendor/_plugins
 //= require_tree ./vendor/plugins/
 //= require bootstrap/affix
-//= require bootstrap/alert
-//= require bootstrap/button
 //= require bootstrap/collapse
-//= require bootstrap/modal
 //= require bootstrap/scrollspy
 //= require bootstrap/tab
 //= require bootstrap/transition
 //= require bootstrap/tooltip
-//= require bootstrap/popover
